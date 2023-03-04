77098d0f1f3804bdaad6aeaa079e3a5b6d8a4059
saiteja2048 <98277450+saiteja2048@users.noreply.github.com>
2023-03-04 22:27:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openPopup() {
+    window.open("file:///C:/Users/saite/OneDrive/Desktop/html/about.html", "popup", "width=600,height=400");
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
